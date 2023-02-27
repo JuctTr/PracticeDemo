dc65364daae155215abbb587e4f8e2e96fca0da3
JuctTr <1301842163@qq.com>
2023-02-27 10:44:05 +0800

    feat(👻): esm

--- ECMAScript6/模块机制/ESM/index.js
+++ ECMAScript6/模块机制/ESM/index.js
@@ -3,3 +3,9 @@ import { counter, incCounter } from './utils.js';
 console.log(counter); // 3
 incCounter();
 console.log(counter); // 4
+
+// console.log(import.meta);
+
+// console.log(new URL(import.meta.url));
+
+// console.log(new URL(import.meta.url).searchParams.get('a'));
